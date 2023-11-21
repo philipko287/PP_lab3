da76357e2fbfabdca7909136321a05423c8306cd
philipko287 <phil.cr728@gmail.com>
2023-11-21 11:40:07 +0200

    Write flask code

--- FLASK.py
+++ FLASK.py
@@ -1,1 +1,9 @@
-print("Hello")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/api/v1/hello-world-1')
+def hello_world():
+    return 'Hello World 1', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
